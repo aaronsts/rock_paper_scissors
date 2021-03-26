16f64faa7bde80e59596ddadef3cd231427aecce
aaron <aaron.staes@gmail.com>
2021-03-26 14:58:34 +0100

    - Created inputPerson function

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-// Ask the user for Input (Case Insensitive)
+// Create a variable and Ask the user for Input (Case Insensitive)
+const inputPerson = () => {
+    let valuePerson = window.prompt('What is your choice (paper, rock or scissor)');
+    valuePerson.toLowerCase();
+    console.log(valuePerson);
+    return valuePerson;
+}
 // Generate inputs for the computer
+const inputComputer = () => {
+    
+}
 // Calculate input for computer
 // Compare both inputs to eachother
 // Decide who won (rock beats scissor, scissor beats paper and paper beats rock)
