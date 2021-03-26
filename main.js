644a89210ce9502a723f9f6a68553d4272117656
aaron <aaron.staes@gmail.com>
2021-03-26 14:51:43 +0100

    - Created pseudo code

---
+++ main.js
@@ -0,0 +1,5 @@
+// Ask the user for Input (Case Insensitive)
+// Generate inputs for the computer
+// Calculate input for computer
+// Compare both inputs to eachother
+// Decide who won (rock beats scissor, scissor beats paper and paper beats rock)
