1e2562775603a75bbd6bf8330290a31f6f5ba3e7
aaron <aaron.staes@gmail.com>
2021-03-26 15:38:27 +0100

    restructured code

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Create a variable and Ask the user for Input (Case Insensitive)
 const inputPerson = () => {
     let valuePerson = window.prompt('What is your choice (paper, rock or scissor)');
-    if (valuePerson != "paper"||"scissor"||"rock"){
-        console.log("Only rock, paper and scissor allowed")
-        return "Only rock, paper and scissor allowed";
-    };
+    // if (valuePerson != "paper"||valuePerson !="scissor"||"rock"){
+    //     console.log("Only rock, paper and scissor allowed")
+    //     return "Only rock, paper and scissor allowed";
+    // };
     valuePerson.toLowerCase();
     console.log(valuePerson);
     return valuePerson;
@@ -24,8 +24,7 @@ const inputComputer = () => {
     return valueComputer;
 }
 // Compare both inputs to eachother & Decide who won (rock beats scissor, scissor beats paper and paper beats rock)
-const compareValues = (person, computer) => {
-    let score = 0;
+const compareValues = (person, computer, score) => {
     switch (person) {
         case "paper":
             if (computer === "rock"){
@@ -33,24 +32,26 @@ const compareValues = (person, computer) => {
                 console.log(1)
                 return ["Paper beats Rock, You won!", score];
             }else if (computer === "scissor"){
+                score -= 1;
                 console.log(2)
-                return "Scissor beats Paper, You lose!";
+                return ["Scissor beats Paper, You lose!", score];
             }else{
                 console.log(3)
-                return "It's a tie!";
+                return ["It's a tie!", score];
             }
             break;
         case "rock":
             if (computer === "scissor"){
                 score += 1;
                 console.log(4)
-                return ["Rock beats Scissor, You won!",score];
+                return ["Rock beats Scissor, You won!", score];
             }else if (computer === "paper"){
+                score -= 1;
                 console.log(5)
-                return "Paper beats Rock, You lose!";
+                return ["Paper beats Rock, You lose!", score];
             }else{
                 console.log(6)
-                return "It's a tie!";
+                return ["It's a tie!", score];
             }
             break;
             case "scissor":
@@ -59,11 +60,12 @@ const compareValues = (person, computer) => {
                     console.log(7)
                     return ["Scissor beats Paper, You won!", score];
                 }else if (computer === "rock"){
+                    score -= 1;
                     console.log(8)
-                    return "Rock beats Scissor, You lose!";
+                    return ["Rock beats Scissor, You lose!", score];
                 }else{
                     console.log(9)
-                    return "It's a tie!";
+                    return ["It's a tie!", score];
                 }
                 break;
         default:
@@ -72,11 +74,14 @@ const compareValues = (person, computer) => {
 };
 // Put all function in main function now
 const rockPaperScissor = () => {
-    let yourScore = 0;
+    let score = 0;
     for (let i = 0; i < 5; i++){
         let person = inputPerson();
         let computer = inputComputer();
-        let outputScore = compareValues(person, computer);
+        let outputArray = compareValues(person, computer, score);
+        let yourScore = outputArray[1];
+        console.log(`Game count: ${i+1}`)
+        console.log(`Your score: ${yourScore}`);
     };
 
 }
